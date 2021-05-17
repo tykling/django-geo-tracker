a6e43a26830de386c7d154d6e7492ba2f195cef3
Thomas Steen Rasmussen <thomas@gibfest.dk>
2021-05-17 16:32:18 +0200

    add timestamp field to location model

--- src/location/models.py
+++ src/location/models.py
@@ -1,9 +1,16 @@
 from django.contrib.gis.db import models
 from django.core.serializers.json import DjangoJSONEncoder
 import uuid
+from django.utils import timezone
 
 class Location(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     tracker = models.ForeignKey("tracker.Tracker", on_delete=models.PROTECT)
     metadata = models.JSONField(encoder=DjangoJSONEncoder, decoder=DjangoJSONEncoder)
     location = models.PointField(editable=True, srid=4326)
+    timestamp = models.DateTimeField(
+        default=timezone.now,
+        help_text="The timestamp of this location",
+    )
+
+
