6e0738090e30e35cd26a80eebf9fde978a936326
Thomas Steen Rasmussen <thomas@gibfest.dk>
2021-06-01 20:14:46 +0200

    add tracker_list url and template

--- src/django_geo_tracker/urls.py
+++ src/django_geo_tracker/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from tracker.views import TrackerDetailView
+from tracker.views import TrackerDetailView, TrackerListView
 from location.views import LocationPostView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tracker/', TrackerListView.as_view(), name="tracker_list"),
     path('tracker/<uuid:tracker_uuid>/', TrackerDetailView.as_view(), name="tracker_detail"),
     path('tracker/<uuid:tracker_uuid>/post_location/', LocationPostView.as_view(), name="post_location"),
 ]
