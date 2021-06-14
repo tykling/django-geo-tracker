de51efbae490aa03595b715d6e3d1565e1bd6e88
Thomas Steen Rasmussen <thomas@gibfest.dk>
2021-06-14 20:49:57 +0200

    uppercase config key names

--- src/tracker/templates/tracker_config.py
+++ src/tracker/templates/tracker_config.py
@@ -2,8 +2,8 @@ CONFIG = {
     "NAME": "{{ name }}",
     "DESCRIPTION": "{{ description }}",
     "LORA": {
-        "country": "EU",
-        "device_address": bytearray([{{ lora_device_address|join:"," }}]),
+        "COUNTRY": "EU",
+        "DEVICE_ADDRESS": bytearray([{{ lora_device_address|join:"," }}]),
         "ABP_NETWORK_SESSION_KEY": bytearray([{{ lora_network_session_key|join:"," }}]),
         "ABP_APPLICATION_SESSION_KEY": bytearray([{{ lora_app_session_key|join:"," }}]),
     },
